a0b3e301b81bce0d50a5b5013b450c5dda99bd47
dang-cong-05 <dangcong21205@gmail.com>
2025-07-21 14:22:19 +0700

    product-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ProductList } from './product-list/product-list';
+import { CategoryList } from './category-list/category-list';
+import { BrandList } from './brand-list/brand-list';
+import { ProductDetail } from './product-detail/product-detail';
+export const routes: Routes = [
+    {path: "",
+     component: ProductList},
+    {path: "products",
+         component: ProductList},
+    {path: "categories",
+        component: CategoryList},
+    {path: "brands",
+        component: BrandList },
+    {path: 'product/:id/detail',
+        component: ProductDetail}
+];
